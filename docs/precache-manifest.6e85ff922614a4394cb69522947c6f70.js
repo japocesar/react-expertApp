ce350d60670d28d7279e669a1d139d6c59ba2a29
Cesar Carrasco <cesar.carrasco.s@hotmail.com>
2020-09-04 12:21:46 -0500

    docs agregado

---
+++ docs/precache-manifest.6e85ff922614a4394cb69522947c6f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6fab421f3545fe826b07b68e3db257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c65e0496b745bb26d41",
+    "url": "/static/css/main.849bb655.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c65e0496b745bb26d41",
+    "url": "/static/js/main.3a50dd89.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
